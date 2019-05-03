d1aa68772f1f7c90427e57b991ccb5ced85063fd
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-03 13:15:23 -0400

    Re add Josh's commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { users: [] };
+  }
+
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
           To get started, edit
-      <code>src/App.js</code>
+          <code>src/App.js</code>
           and save to reload.
-    </p>
-  </div>
-);
+        </p>
+        <h1>
+          {users.map(user => <div key={user.id}>{user.username}</div>)}
+        </h1>
+      </div>
+    );
+  }
+}
 
 export default App;
