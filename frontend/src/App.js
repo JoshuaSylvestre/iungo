592aa8c34e6c5bf5c8695b663c0d2ffd966a1894
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-03 13:19:55 -0400

    Fix oopsie

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { users: [] };
-  }
+  state = {users: []}
 
   componentDidMount() {
     fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+    .then(res => res.json())
+    .then(users => this.setState({ users }));
   }
-
+  
   render() {
-    const { users } = this.state;
-
     return (
       <div className="App">
         <div className="App-header">
@@ -24,12 +19,12 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>
-          {users.map(user => <div key={user.id}>{user.username}</div>)}
+          {this.state.users.map(user =>
+            <div key={user.id}>{user.username}</div>
+          )}
         </h1>
       </div>
     );
