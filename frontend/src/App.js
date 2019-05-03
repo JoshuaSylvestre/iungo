3db91a8cc1ff33293982453e35a8e13b354521ac
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-03 13:46:56 -0400

    Re-re-add constructor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {users: []}
+  constructor() {
+    super();
+    this.state = { users: [] };
+  }
 
   componentDidMount() {
     fetch('/users')
-    .then(res => res.json())
-    .then(users => this.setState({ users }));
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
   }
-  
+
   render() {
+    const { users } = this.state;
     return (
       <div className="App">
         <div className="App-header">
@@ -19,12 +23,12 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <h1>
-          {this.state.users.map(user =>
-            <div key={user.id}>{user.username}</div>
-          )}
+          {users.map(user => <div key={user.id}>{user.username}</div>)}
         </h1>
       </div>
     );
