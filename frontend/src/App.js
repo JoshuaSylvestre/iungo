b3059990242eb962d6c3d5811add61fa8c5ca392
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-04 07:49:47 -0400

    Add Landing to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Landing from './components/pages/Landing';
 import './App.css';
 
 class App extends Component {
@@ -18,15 +18,8 @@ class App extends Component {
     const { users } = this.state;
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
+
+        <Landing />
         <h1>
           {users.map(user => <div key={user.id}>{user.username}</div>)}
         </h1>
