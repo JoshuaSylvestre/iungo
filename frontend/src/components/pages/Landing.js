99187e1eee1840acd25c187a370b6371f875012f
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-04 10:49:18 -0400

    Add some bulma/sass setup

---
+++ frontend/src/components/pages/Landing.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+const styles = {
+  hero: { height: '100vh', width: '100vw', objectFit: 'cover' },
+};
+
+
+export default class Landing extends Component {
+  constructor() {
+    super();
+    
+    this.state = { user: null };
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
