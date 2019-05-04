2d44264536d3cb24f9d95ca11c5791d85a948668
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-04 07:48:51 -0400

    Refactor Landing

--- frontend/src/components/pages/Landing.js
+++ frontend/src/components/pages/Landing.js
@@ -1,21 +1,76 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  Hero, Navbar, Container, Button, Title, Tab,
+  // Icon, FontAwesomeIcon,
+} from 'rbx';
 
 
+const handsUrl = 'https://images.pexels.com/photos/943630/pexels-photo-943630.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260';
 const styles = {
-  hero: { height: '100vh', width: '100vw', objectFit: 'cover' },
+  hero: {
+    backgroundImage: `url(${handsUrl})`,
+    opacity: 0.9,
+    color: 'black',
+    backgroundSize: 'cover',
+    overflow: 'hidden',
+  },
+
 };
+const Landing = () => (
+  <Hero as="div" size="fullheight">
+    {/* Hero head: will stick at the top */}
 
+    <Hero.Head>
+      <Navbar>
+        <Container>
+          <Navbar.Brand>
+            <Navbar.Item>
+              <Title>IUNGO</Title>
+            </Navbar.Item>
+            <Navbar.Burger />
+          </Navbar.Brand>
+          <Navbar.Menu>
+            <Navbar.Segment align="end">
+              <Navbar.Item active>Home</Navbar.Item>
+              <Navbar.Item>Examples</Navbar.Item>
+              <Navbar.Item>Documentation</Navbar.Item>
+              <Navbar.Item as="span">
+                <Button color="primary" inverted>
+                  {/* <Icon>
+                    <FontAwesomeIcon icon={faGithub} />
+                  </Icon> */}
+                  <span>Download</span>
+                </Button>
+              </Navbar.Item>
+            </Navbar.Segment>
+          </Navbar.Menu>
+        </Container>
+      </Navbar>
 
-export default class Landing extends Component {
-  constructor() {
-    super();
-    
-    this.state = { user: null };
-  }
+    </Hero.Head>
+    {/* Hero content: will be in the middle */}
+    <Hero.Body style={styles.hero} title="Connection image">
+      <Container textAlign="centered">
+        <Title style={{ color: 'black' }}>Iungo</Title>
+        <Title style={{ color: 'black' }} as="h2" subtitle>
+        Come connect.
+        </Title>
+      </Container>
+    </Hero.Body>
 
-  render() {
-    return (
+    {/* Hero footer: will stick at the bottom */}
+    <Hero.Foot>
+      <Container>
+        <Tab.Group as="nav" type="boxed" fullwidth>
+          <Tab active>Overview</Tab>
+          <Tab>Languages</Tab>
+          <Tab>Technologies</Tab>
+          <Tab>Contribute</Tab>
+          <Tab>Support</Tab>
+        </Tab.Group>
+      </Container>
+    </Hero.Foot>
+  </Hero>
+);
 
-    );
-  }
-}
+export default Landing;
