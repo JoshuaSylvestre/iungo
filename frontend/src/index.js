1ef65b2f8cef3bed324a6c8164d081ea2a1c7dda
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-04 07:47:32 -0400

    Add rbx

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'rbx/index.css';
+
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
