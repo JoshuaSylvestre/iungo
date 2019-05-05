9456218d1988e4f6c6fb4ba69ca30de4dfc14e5a
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-05 07:15:08 -0400

    Reduce App to just the final transcript

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,9 @@
 import React, { Component } from 'react';
 import SpeechRecognition from 'react-speech-recognition';
 import translate from 'translate';
-import logo from './logo.svg';
-import './App.css';
 
 const options = {
-  autoStart: false,
+  autoStart: true,
   continuous: true
 }
 
@@ -19,30 +17,16 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // fetch('/users')
-    //   .then(res => res.json())
-    //   .then(users => this.setState({ users }));
     this.props.recognition.lang = 'en';
   }
 
   render() {
     const { users } = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
+      <div className="App">        
+      <h1>Start speaking, your text will appear on the screen below:</h1>
         <h1>
-          {/* {users.map(user => <div key={user.id}>{user.username}</div>)} */}
-          <button onClick={this.props.startListening}>Start</button>
-          <button onClick={this.props.stopListening}>Stop</button>
-          <span>{this.props.finalTranscript}</span>
+          <span style={{color: 'black'}}>{this.props.finalTranscript}</span>
         </h1>
       </div>
     );
