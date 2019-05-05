5a3bf06be0c6edb3ab1b7ebb2e86036904868ef4
Joshua Sylvestre <joshuasylvestre@knights.ucf.edu>
2019-05-05 06:02:50 -0400

    Adds speech recognition and removes the video and disctophone components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
 /* eslint-disable linebreak-style */
 import React, { Component } from 'react';
-import VideoComponent from './components/VideoComponent';
-import Dictaphone from './components/Dictaphone';
+import SpeechRecognition from 'react-speech-recognition';
+import translate from 'translate';
 import logo from './logo.svg';
 import './App.css';
+
+const options = {
+  autoStart: false,
+  continuous: true
+}
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { users: [] };
+    this.state = { 
+      users: []
+    };
   }
 
   componentDidMount() {
     // fetch('/users')
     //   .then(res => res.json())
     //   .then(users => this.setState({ users }));
+    this.props.recognition.lang = 'en';
   }
 
   render() {
@@ -32,12 +40,13 @@ class App extends Component {
         </p>
         <h1>
           {/* {users.map(user => <div key={user.id}>{user.username}</div>)} */}
-          <VideoComponent />
-          <Dictaphone />
+          <button onClick={this.props.startListening}>Start</button>
+          <button onClick={this.props.stopListening}>Stop</button>
+          <span>{this.props.finalTranscript}</span>
         </h1>
       </div>
     );
   }
 }
 
-export default App;
+export default  SpeechRecognition(options)(App);
