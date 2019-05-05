ec02a0cc7d9b17be2a94691999adef1c3377195c
Steeve Joseph <steevejoseph@knights.ucf.edu>
2019-05-05 06:56:17 -0400

    Rename src to old_src

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './containers/HomePage'
+import Room from './containers/RoomPage'
+import NotFound from './components/NotFound'
+import styles from './app.css'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/r/:room" component={Room} />
+				<Route path="*" component={NotFound} />
+			</Switch>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root')
 );
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,1 +1,12 @@
-// Gotta remember to combine reducers here
+import { combineReducers } from 'redux';
+// Reducers
+import roomReducer from './room-reducer';
+import audioReducer from './audio-reducer';
+import videoReducer from './video-reducer';
+// Combine Reducers
+const reducers = combineReducers({
+  rooms: roomReducer,
+  video: videoReducer,
+  audio: audioReducer
+});
+export default reducers;
